3491ea7f2f3d21e840b3bfcd0d68ef1ba2c4083d
Erioifpud <erioifpud@zuo.si>
2018-10-30 23:56:36 +0800

    18.10.30 更新

--- frontend/simple-app/src/components/DataPage.vue
+++ frontend/simple-app/src/components/DataPage.vue
@@ -32,61 +32,98 @@
         <div slot="title">
           <x-switch title="最小值" v-model="needMin"></x-switch>
         </div>
-        <x-input v-model="min" type="number" :disabled="!needMin" placeholder="min"></x-input>
+        <!-- <x-input v-model="min" type="number" :disabled="!needMin" placeholder="min"></x-input> -->
+        <x-number title="" v-model="min" fillable></x-number>
       </cell>
       <cell>
         <div slot="title">
           <x-switch title="最大值" v-model="needMax"></x-switch>
         </div>
-        <x-input v-model="max" type="number" :disabled="!needMax" placeholder="max"></x-input>
+        <!-- <x-input v-model="max" type="number" :disabled="!needMax" placeholder="max"></x-input> -->
+        <x-number title="" v-model="max" fillable></x-number>
       </cell>
       <!-- 日期 -->
       <!-- TODO: state -->
       <cell>
         <div slot="title">
-          <x-switch title="起始" v-model="needMin"></x-switch>
+          <x-switch title="起始" v-model="needStartDate"></x-switch>
         </div>
-        <datetime-range title="" start-date="2017-01-01" end-date="2017-02-02" format="YYYY/MM/DD" v-model="startDate"></datetime-range>
+        <datetime-range title="" :start-date="`${new Date().getFullYear()}-01-01`" :end-date="`${new Date().getFullYear()}-12-31`" format="YYYY/MM/DD" v-model="startDate"></datetime-range>
       </cell>
       <cell>
         <div slot="title">
-          <x-switch title="终止" v-model="needMin"></x-switch>
+          <x-switch title="终止" v-model="needEndDate"></x-switch>
         </div>
-        <datetime-range title="" start-date="2017-01-01" end-date="2017-02-02" format="YYYY/MM/DD" v-model="endDate"></datetime-range>
+        <datetime-range title="" :start-date="`${new Date().getFullYear()}-01-01`" :end-date="`${new Date().getFullYear()}-12-31`" format="YYYY/MM/DD" v-model="endDate"></datetime-range>
       </cell>
     </group>
 
+    <divider>查询结果</divider>
+    <timeline class="timeline">
+			<timeline-item>
+				<h4 class="recent">【广东】 广州市 已发出</h4>
+				<p class="recent">2016-04-17 12:00:00</p>
+			</timeline-item>
+			<timeline-item>
+				<h4> 申通快递员 广东广州 收件员 xxx 已揽件</h4>
+				<p>2016-04-16 10:23:00</p>
+			</timeline-item>
+			<timeline-item>
+				<h4> 商家正在通知快递公司揽件</h4>
+				<p>2016-04-15 9:00:00</p>
+			</timeline-item>
+      <timeline-item>
+				<h4> 商家正在通知快递公司揽件</h4>
+				<p>2016-04-15 9:00:00</p>
+			</timeline-item>
+      <timeline-item>
+				<h4> 商家正在通知快递公司揽件</h4>
+				<p>2016-04-15 9:00:00</p>
+			</timeline-item>
+      <timeline-item>
+				<h4> 商家正在通知快递公司揽件</h4>
+				<p>2016-04-15 9:00:00</p>
+			</timeline-item>
+      <timeline-item>
+				<h4> 商家正在通知快递公司揽件</h4>
+				<p>2016-04-15 9:00:00</p>
+			</timeline-item>
+		</timeline>
   </div>
 </template>
 
 <script>
 import mixin from '@/mixin'
-import { Card, Box, DatetimeRange, XSwitch, XInput, Timeline, Group, Range, Cell, XButton } from 'vux'
+import { Card, DatetimeRange, XSwitch, XNumber, Timeline, TimelineItem, Group, Range, Cell, XButton, Divider } from 'vux'
+import moment from 'moment'
 
 export default {
   mixins: [mixin.updateBar],
   components: {
     Card,
-    Box,
     DatetimeRange,
     XSwitch,
-    XInput,
+    XNumber,
     Timeline,
+    TimelineItem,
     Group,
     Range,
     Cell,
-    XButton
+    XButton,
+    Divider
   },
   data () {
     return {
       title: '数据点',
       limit: 10,
       needMin: false,
-      min: '',
+      min: 0,
       needMax: false,
-      max: '',
-      startDate: ['2017-01-15', '03', '05'],
-      endDate: ['2017-01-15', '03', '05']
+      max: 0,
+      needStartDate: false,
+      startDate: [moment().format('YYYY-MM-DD'), '00', '00'],
+      needEndDate: false,
+      endDate: [moment().format('YYYY-MM-DD'), '23', '59']
     }
   },
   methods: {
@@ -124,5 +161,19 @@ export default {
 }
 .card-demo-flex span {
   color: #f74c31;
+}
+
+.timeline {
+	p {
+		color: #888;
+		font-size: 0.8rem;
+	}
+	h4 {
+		color: #666;
+		font-weight: normal;
+	}
+	.recent {
+		color: rgb(4, 190, 2)
+	}
 }
 </style>
